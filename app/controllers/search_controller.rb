20659bb32586dbcce6ffd4208c02473b78504a2c
Matt Pindell <pindell.m@gmail.com>
2016-06-06 09:40:45 -0600

    Advancement on Faraday request

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,6 @@
 class SearchController < ApplicationController
 
   def index
-    # conn.get '/nigiri', search_params
     require "pry"
     binding.pry
   end
@@ -9,11 +8,15 @@ class SearchController < ApplicationController
   private
 
     def connection
-      url = "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=1&api_key=#{ENV['API_KEY']}"
+      url = "https://api.data.gov/nrel/alt-fuel-stations/v1.json?limit=6&api_key=#{ENV['API_KEY']}"
       Faraday.new(url: url)
     end
 
     def search_params
       params.permit('q')
+    end
+
+    def search
+      get 'https://api.data.gov/nrel/alt-fuel-stations/v1.json?api_key=ttRf0e4JymfK05atNs8xiHEzc7QAHUzigZKTvAYF&limit=6&location=80202&fuel=ELEC&&fuel=LPG&private=false&owner=all&radius=false&radius_miles=6'
     end
 end
