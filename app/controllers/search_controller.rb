667826c66913998e4ae6966d931b40cfed357731
Matt Pindell <pindell.m@gmail.com>
2016-06-06 10:42:40 -0600

    Wow.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -23,8 +23,8 @@ class SearchController < ApplicationController
         req.params['api_key']      = ENV['API_KEY']
         req.params['limit']        = 10
         req.params['location']     = search_params['q']
-        req.params['fuel']         = 'ELEC'
-        req.params['fuel']         = 'LPG'
+        req.params['fuel_type']    = 'ELEC'
+        req.params['fuel_type']    = 'LPG'
         req.params['private']      = false
         req.params['radius_miles'] = 6
       end
